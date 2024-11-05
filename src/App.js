d7577e548520e5a43d4c6d8f6cd12941e801e165
Arkajyoti Kundu <arkajyotikundu415@gmail.com>
2024-11-05 11:52:50 +0530

    create errror page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-import Navbar from './Components/Navbar/Navbar';
-import Footer from './Components/Footer/Footer';
+
 import Home from './Components/Home/Home';
+
+
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+
       <Home/>
-      <Footer/>
+      
     </div>
   );
 }
