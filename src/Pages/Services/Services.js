6f4cc0f03c826d6b89d8a325ed01bca6b04942c2
Soumyajit Ray <soumyajitray2002@gmail.com>
2024-11-08 16:50:07 +0530

    update services page

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import './Services.css';
-import logo from '../../Assets/logo.png'
+import logo from '../../Assets/logo.png';
+
 const services = [
-  { title: 'Baby Shower Catering', image: logo  },
-  { title: 'Office Catering Service', image: logo  },
-  { title: 'Housewarming Catering', image: logo },
-  { title: 'Birthday Party Catering', image: logo },
-  { title: 'Anniversary Catering', image: logo},
-  { title: 'Bengali Food Catering', image: logo},
-  { title: 'Rice Ceremony Catering', image: logo },
-  { title: 'Engagement Party Catering', image:logo  },
-  { title: 'Reunion Party' , image : logo}
+  { title: 'Event Planning & Coordination', image: logo },
+  { title: 'Event Consultancy', image: logo },
+  { title: 'Catering Services', image: logo },
+  { title: 'Theme Decoration', image: logo },
+  { title: 'Light & Sound', image: logo },
+  { title: 'DJ', image: logo },
+  { title: 'Live Band', image: logo },
+  { title: 'Choreographer', image: logo },
+  { title: 'SFX', image: logo },
+  { title: 'Photographer', image: logo },
+  { title: 'Makeup Artist', image: logo },
+  { title: 'Event Security', image: logo },
+  { title: 'Hire-Car for Event', image: logo },
 ];
 
 function ServiceComponent() {
   return (
     <div className="service-container">
-      <h2>Discover Our Different Types Of Catering Services</h2>
+      <h2>Discover Our Exclusive Catering Services</h2>
       <div className="service-grid">
         {services.map((service, index) => (
           <div className="service-card" key={index}>
-            <img src={service.image} alt={service.title} className="service-image" />
+            <div className="service-image-wrapper">
+              <img src={service.image} alt={service.title} className="service-image" />
+            </div>
             <h3 className="service-title">{service.title}</h3>
           </div>
         ))}
