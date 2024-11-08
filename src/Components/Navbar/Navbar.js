3631e0345e16af3750a48fe06886c1a07da37d0a
Soumyajit Ray <soumyajitray2002@gmail.com>
2024-11-08 16:51:30 +0530

    update navbar section

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,54 +1,102 @@
-import React, { useState } from 'react';
-import './Navbar.css';
-import Hamburger from 'hamburger-react';
-import { Link, NavLink } from 'react-router-dom';
-
+import React, { useState } from "react";
+import "./Navbar.css";
+import Hamburger from "hamburger-react";
+import { Link, NavLink } from "react-router-dom";
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    return (
-        <nav className="navbar">
-            <div className="navbar-left">
-                <Link to="/"> <img src="logo.png" alt="Avishree-Logo" className="logo" /></Link>
-                <div className="brand">
-                <NavLink to="/"> <h1>Avishree Hospitality Pvt. Ltd.</h1></NavLink>
-                    <p>crafting unfogettable Memories</p>
-                </div>
-            </div>
+  return (
+    <nav className="navbar">
+      <div className="navbar-left ">
+        <Link to="/">  
+          <img src="logo.png" alt="Avishree-Logo" className="logo" />
+        </Link>
+        <div className="brand">
+          <NavLink to="/">
+           
+            <h1>Avishree Hospitality Pvt. Ltd.</h1>
+          </NavLink>
+          <p>Crafting unfogettable Memories</p>
+        </div>
+      </div>
 
-            {/* Desktop Menu */}
-            <div className="desktop-menu">
-                <ul className="navbar-menu">
-                    <li><NavLink to="/">Home</NavLink></li>
-                    <li><NavLink to="/AboutUs">About Us</NavLink></li>
-                    <li><NavLink to="/services">Services</NavLink></li>
-                    <li><NavLink to="/menu">Menu</NavLink></li>
-                    <li><NavLink to="/gallery">Gallery</NavLink></li>
-                    <li><NavLink to="/contact">Contact Us</NavLink></li>
-                    <li><NavLink to="/blogs">Blogs</NavLink></li>
-                </ul>
-                <NavLink to="/book" className="book-now-button">Book Now</NavLink>
-            </div>
+      {/* Desktop Menu */}
+      <div className="desktop-menu">
+        <ul className="navbar-menu">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/AboutUs">About Us</NavLink>
+          </li>
+          <li>
+            <NavLink to="/services">Services</NavLink>
+          </li>
+          <li>
+            <NavLink to="/menu">Menu</NavLink>
+          </li>
+          <li className="nav-item gallery-dropdown">
+            <NavLink to="/gallery">Gallery</NavLink>
+            <ArrowDropDownIcon/>
+            <ul className="dropdown bg-transparent">
+              <li>
+                <NavLink to="/gallery/videos">Video</NavLink>
+              </li>
+              <li>
+                <NavLink to="/gallery/photos">Photo</NavLink>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact Us</NavLink>
+          </li>
+          <li>
+            <NavLink to="/blogs">Blogs</NavLink>
+          </li>
+        </ul>
+        <NavLink to="/book" className="book-now-button">
+          Book Now
+        </NavLink>
+      </div>
 
-            {/* Mobile Menu */}
-            <div className="mobile-menu">
-                <Hamburger toggled={isOpen} toggle={setIsOpen} />
-                {isOpen && (
-                    <ul className="navbar-menu-mobile z-10 ">
-                        <li><NavLink to="/">Home</NavLink></li>
-                        <li><NavLink to="/AboutUs">About Us</NavLink></li>
-                        <li><NavLink to="/services">Services</NavLink></li>
-                        <li><NavLink to="/menu">Menu</NavLink></li>
-                        <li><NavLink to="/gallery">Gallery</NavLink></li>
-                        <li><NavLink to="/contact">Contact Us</NavLink></li>
-                        <li><NavLink to="/blogs">Blogs</NavLink></li>
-                        <li><NavLink to="/book" className="book-now-button-mobile">Book Now</NavLink></li>
-                    </ul>
-                )}
-            </div>
-        </nav>
-    );
+      {/* Mobile Menu */}
+      <div className="mobile-menu  border-2 ">
+        <Hamburger toggled={isOpen} toggle={setIsOpen} />
+        {isOpen && (
+          <ul className="navbar-menu-mobile z-10 ">
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/AboutUs">About Us</NavLink>
+            </li>
+            <li>
+              <NavLink to="/services">Services</NavLink>
+            </li>
+            <li>
+              <NavLink to="/menu">Menu</NavLink>
+            </li>
+            <li>
+              <NavLink to="/gallery">Gallery</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Contact Us</NavLink>
+            </li>
+            <li>
+              <NavLink to="/blogs">Blogs</NavLink>
+            </li>
+            <li>
+              <NavLink to="/book" className="book-now-button-mobile">
+                Book Now
+              </NavLink>
+            </li>
+          </ul>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
