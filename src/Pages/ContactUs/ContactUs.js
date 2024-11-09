fd867973d81fee24bcee416388ae969da24e62e0
Soumyajit Ray <soumyajitray2002@gmail.com>
2024-11-10 02:06:00 +0530

    "Updated ContactUs.js: reformatted Follow Us section, removed iframe and adjusted div layout"

--- src/Pages/ContactUs/ContactUs.js
+++ src/Pages/ContactUs/ContactUs.js
@@ -32,8 +32,9 @@ const ContactUs = () => {
         
         <button type="submit">Send Message</button>
       </form>
-      <div className="contact-options mt-10">
-        <h1>Follow Us: </h1>
+      <div className='mt-10'> <h1>Follow Us On: </h1></div>   
+      <div className="contact-options ">
+   
         <a href="https://www.youtube.com/@Avishree-241"  className="contact-link youtube " target="_blank"  rel="noopener noreferrer">
          <YouTubeIcon/>
         </a>
@@ -53,18 +54,7 @@ const ContactUs = () => {
          <LinkIcon />
         </a>
       </div>
-      <div className=' lg:w-1/2 mx-0 lg:m-5 h-[80%] flex   w-full  pt-10 overflow-hidden' >
-      {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13329.777848284057!2d88.46299531861573!3d22.63622114887646!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f89f77bdfa1d47%3A0x6f9670a752dda326!2sParadise%20Car%20Rental!5e1!3m2!1sen!2sin!4v1726224121573!5m2!1sen!2sin" 
-        title='office'
-        width="630" 
-        height="380" 
-        // style="border:0" 
-        allowFullScreen 
-        loading="lazy" 
-        referrerPolicy="no-referrer" 
-        className='border-2 border-gray-400 rounded-lg shadow-md transition-all duration-300 ease-in-out  hover:scale-95'>
-        </iframe> */}
-    </div>
+    
      
     </div>
     </>
