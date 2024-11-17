b555969bd330723f2b1d92ce78158d308520f844
Soumyajit Ray <raysoumajit@outlook.com>
2024-11-17 22:40:44 +0530

    ###Bug/Fixed fibrary for gallareynaly ready to install li

--- src/Components/expand/expand.js
+++ src/Components/expand/expand.js
@@ -1,12 +1,11 @@
 import  { useState } from 'react';
 import { FaPlus, FaMinus } from 'react-icons/fa';
-import PropTypes from 'prop-types';
 
 
 
-{/* eslint-disable-next-line react/no-unescaped-entities */}
+
 const Expand = ({ question, children }) => {
-  {/* eslint-disable-next-line react/no-unescaped-entities */}
+
   const [isExpanded, setIsExpanded] = useState(false);
 
   const handleToggle = () => {
@@ -32,8 +31,5 @@ const Expand = ({ question, children }) => {
   );
 };
 
-Expand.propTypes = {
-  question: PropTypes.string.isRequired,
-  children: PropTypes.node.isRequired,
-};
+
 export default Expand;
