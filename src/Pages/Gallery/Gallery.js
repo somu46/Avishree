51c3c9bb5657bf578855424e35338da076ad853c
Arkajyoti Kundu <arkajyotikundu415@gmail.com>
2024-11-21 09:18:16 +0530

    update light galarey  config and fixed bug

--- src/Pages/Gallery/Gallery.js
+++ src/Pages/Gallery/Gallery.js
@@ -1,28 +1,58 @@
-import React from 'react';
-import ImageGallery from 'react-image-gallery';
-import 'react-image-gallery/styles/css/image-gallery.css';
-import './GalleryStyles.css';
-import PhotosData from '../Photos/PhotoData';
+import React, { useEffect } from 'react';
+import Masonry from 'masonry-layout';
+import imagesLoaded from 'imagesloaded';
+import LightGallery from 'lightgallery/react';
+import lgZoom from 'lightgallery/plugins/zoom';
+import lgShare from 'lightgallery/plugins/share';
+import lgHash from 'lightgallery/plugins/hash';
+
+import PhotosData from '../Photos/PhotoData'; 
+import './GalleryStyles.scss'; 
 
 
 
 const GalleryComponent = () => {
+  useEffect(() => {
+    const container = document.querySelector('.masonry-gallery-demo');
+    if (container) {
+      
+      const msnry = new Masonry(container, {
+        itemSelector: '.gallery-item',
+        columnWidth: '.grid-sizer',
+        percentPosition: true,
+      });
+
+      
+      imagesLoaded(container).on('progress', () => {
+        msnry.layout();
+      });
+    }
+  }, []);
+
   return (
-    <div className="min-h-screen bg-white py-10 mt-[5.1rem]">
-      <h2 className="text-center text-3xl md:text-4xl text-gold font-bold mb-10">Our Elegant Gallery</h2>
-      <div className="max-w-6xl mx-auto bg-gray-100 rounded-xl shadow-lg p-8">
-        <ImageGallery
-          items={PhotosData}
-          autoPlay={true}
-          showThumbnails={true}
-          showFullscreenButton={true}
-          showPlayButton={true}
-          slideInterval={3000}
-          showBullets={true}
-          infinite={true}
-          thumbnailPosition="bottom"
-        />
-      </div>
+    <div className="mt-[5.3rem] border border-red-500 p-3 h-screen">
+      <LightGallery
+        elementClassNames="masonry-gallery-demo  "
+        plugins={[lgZoom, lgShare, lgHash]}
+        speed={500}
+      >
+        <div className=" flex  border border-blue-500 h-auto m-1"></div>
+        {PhotosData.map((photo) => (
+          <a
+            key={photo.id}
+            href={photo.original}
+            data-lg-size="1600-1067"
+            className=" border border-yellow-600"
+            data-src={photo.original}
+          >
+            <img
+              alt="Gallery poster"
+              className="img-responsive border border-green-500"
+              src={photo.thumbnail}
+            />
+          </a>
+        ))}
+      </LightGallery>
     </div>
   );
 };
--- src/Pages/Photos/PhotoData.js
+++ src/Pages/Photos/PhotoData.js
@@ -1,24 +1,25 @@
 const PhotosData = [
     {
       id: 1,
-      original: 'https://drive.google.com/file/d/1Z4z24M0P7Mq8tjO2UKJVn_WfCuyVNKq3/view?usp=drive_link',
-      thumbnail: 'https://drive.google.com/file/d/1Z4z24M0P7Mq8tjO2UKJVn_WfCuyVNKq3/view?usp=drive_link',
-    },
-    {
-      id: 2,
-      original: 'https://drive.google.com/file/d/1255frrmiXYsAwQfS7cCf7-esGJPNzYNp/view?usp=sharing',
-      thumbnail: 'https://drive.google.com/file/d/1255frrmiXYsAwQfS7cCf7-esGJPNzYNp/view?usp=sharing',
-    },
-    {
-      id: 3,
       original: 'https://images.pexels.com/photos/3392093/pexels-photo-3392093.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
       thumbnail: 'https://images.pexels.com/photos/3392093/pexels-photo-3392093.jpeg?auto=compress&cs=tinysrgb&w=150',
     },
     {
-      id: 4,
+      id: 2,
       original: 'https://images.pexels.com/photos/1756640/pexels-photo-1756640.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
       thumbnail: 'https://images.pexels.com/photos/1756640/pexels-photo-1756640.jpeg?auto=compress&cs=tinysrgb&w=150',
     },
+    {
+      id: 3,
+      original: 'https://images.pexels.com/photos/1643383/pexels-photo-1643383.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      thumbnail: 'https://images.pexels.com/photos/1643383/pexels-photo-1643383.jpeg?auto=compress&cs=tinysrgb&w=150',
+    },
+    {
+      id: 4,
+      original: 'https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      thumbnail: 'https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=150',
+    },
   ];
-
+  
   export default PhotosData;
+  
