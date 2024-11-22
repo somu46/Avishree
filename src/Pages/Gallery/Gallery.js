52e7ec4dfeb6d4cd9e5c5812865919c07aef777d
Arkajyoti Kundu <arkajyotikundu415@gmail.com>
2024-11-22 09:51:26 +0530

    "Refactored AboutUs and Gallery components, updated Gallery to use LightGallery with additional plugins, and modified corresponding styles in GalleryStyles.scss."

--- src/Pages/Gallery/Gallery.js
+++ src/Pages/Gallery/Gallery.js
@@ -1,60 +1,106 @@
-import React, { useEffect } from 'react';
-import Masonry from 'masonry-layout';
-import imagesLoaded from 'imagesloaded';
-import LightGallery from 'lightgallery/react';
-import lgZoom from 'lightgallery/plugins/zoom';
-import lgShare from 'lightgallery/plugins/share';
-import lgHash from 'lightgallery/plugins/hash';
-
 import PhotosData from '../Photos/PhotoData'; 
 import './GalleryStyles.scss'; 
 
 
+import LightGallery from 'lightgallery/react';
 
-const GalleryComponent = () => {
-  useEffect(() => {
-    const container = document.querySelector('.masonry-gallery-demo');
-    if (container) {
+
+
+
+// import plugins if you need
+import lgThumbnail from 'lightgallery/plugins/thumbnail';
+import lgZoom from 'lightgallery/plugins/zoom';
+import lgAutoplay from 'lightgallery/plugins/autoplay'
+import lgFullscreen from 'lightgallery/plugins/fullscreen';
+import lgShare from 'lightgallery/plugins/share';
+import lgRotate from 'lightgallery/plugins/rotate';
+
+
+
+
+
+
+function GalleryComponent() {
+    const onInit = () => {
+        console.log('lightGallery has been initialized');
+    };
+    return (
+        <div className="mt-[5.1rem] app">
+            <LightGallery
+                onInit={onInit}
+                speed={500}
+                plugins={[lgThumbnail, lgZoom, lgAutoplay, lgFullscreen, lgRotate, lgShare]}
+            >
+
+                {PhotosData.map((image, index) => {
+                    return (
+                        <a href={image.original} key={index}>
+                            <img alt={image.alt} src={image.thumbnail} />
+                        </a>
+                    )
+                })}
+
+
+            </LightGallery>
+        </div>
+    );
+}
+
+// import React, { useEffect } from 'react';
+// import Masonry from 'masonry-layout';
+// import imagesLoaded from 'imagesloaded';
+// import LightGallery from 'lightgallery/react';
+// import lgZoom from 'lightgallery/plugins/zoom';
+// import lgShare from 'lightgallery/plugins/share';
+// import lgHash from 'lightgallery/plugins/hash';
+
+
+
+
+// const GalleryComponent = () => {
+//   useEffect(() => {
+//     const container = document.querySelector('.masonry-gallery-demo');
+//     if (container) {
       
-      const msnry = new Masonry(container, {
-        itemSelector: '.gallery-item',
-        columnWidth: '.grid-sizer',
-        percentPosition: true,
-      });
+//       const msnry = new Masonry(container, {
+//         itemSelector: '.gallery-item',
+//         columnWidth: '.grid-sizer',
+//         percentPosition: true,
+//       });
 
       
-      imagesLoaded(container).on('progress', () => {
-        msnry.layout();
-      });
-    }
-  }, []);
+//       imagesLoaded(container).on('progress', () => {
+//         msnry.layout();
+//       });
+//     }
+//   }, []);
 
-  return (
-    <div className="mt-[5.3rem] border border-red-500 p-3 h-screen">
-      <LightGallery
-        elementClassNames="masonry-gallery-demo  "
-        plugins={[lgZoom, lgShare, lgHash]}
-        speed={500}
-      >
-        <div className=" flex  border border-blue-500 h-auto m-1"></div>
-        {PhotosData.map((photo) => (
-          <a
-            key={photo.id}
-            href={photo.original}
-            data-lg-size="1600-1067"
-            className=" border border-yellow-600"
-            data-src={photo.original}
-          >
-            <img
-              alt="Gallery poster"
-              className="img-responsive border border-green-500"
-              src={photo.thumbnail}
-            />
-          </a>
-        ))}
-      </LightGallery>
-    </div>
-  );
-};
+//   return (
+//     <div className="mt-[5.3rem] border border-red-500 p-3 h-screen">
+//       <LightGallery
+//         elementClassNames="masonry-gallery-demo  "
+//         plugins={[lgZoom, lgShare, lgHash]}
+//         speed={500}
+//       >
+//         <div className=" flex  border border-blue-500 h-auto m-1"></div>
+//         {PhotosData.map((photo) => (
+//           <a
+//             key={photo.id}
+//             href={photo.original}
+//             data-lg-size="1600-1067"
+//             className=" border border-yellow-600"
+//             data-src={photo.original}
+//           >
+//             <img
+//               alt="Gallery poster"
+//               className="img-responsive border border-green-500"
+//               src={photo.thumbnail}
+//             />
+//           </a>
+//         ))}
+//       </LightGallery>
+//     </div>
+//   );
+// };
 
 export default GalleryComponent;
