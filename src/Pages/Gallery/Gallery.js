3370ab2731c1871ea458726b6c0bdea33ae334cb
Arkajyoti Kundu <arkajyotikundu415@gmail.com>
2024-11-23 15:15:28 +0530

    "Updated BanquetHalls.js and Gallery.js components, removed unused import in Gallery.js, and modified GalleryStyles.scss successfuly retrive data from google drive "

--- src/Pages/Gallery/Gallery.js
+++ src/Pages/Gallery/Gallery.js
@@ -1,22 +1,11 @@
-// import PhotosData from '../Photos/PhotoData'; 
 import './GalleryStyles.scss'; 
-
-
 import LightGallery from 'lightgallery/react';
-
-
-
-
-// import plugins if you need
 import lgThumbnail from 'lightgallery/plugins/thumbnail';
 import lgZoom from 'lightgallery/plugins/zoom';
-import lgAutoplay from 'lightgallery/plugins/autoplay'
+import lgAutoplay from 'lightgallery/plugins/autoplay';
 import lgFullscreen from 'lightgallery/plugins/fullscreen';
 import lgShare from 'lightgallery/plugins/share';
 import lgRotate from 'lightgallery/plugins/rotate';
-
-
-
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -24,16 +13,16 @@ function GalleryComponent() {
     const [photosData, setPhotosData] = useState([]);
 
     useEffect(() => {
-        // Function to fetch photos from Google Drive
         const fetchPhotos = async () => {
-            const apiKey = 'YOUR_API_KEY';
-            const folderId = 'YOUR_FOLDER_ID';
+            const apiKey = 'AIzaSyClV3dkL-ZsWjmhQoacCppPO7L9fxB3wio';
+            const folderId = '15vfI5ZXeUHahBmptmo6x_B6PA4jqmNnI';
 
             try {
                 const response = await axios.get(
                     `https://www.googleapis.com/drive/v3/files?q='${folderId}'+in+parents&key=${apiKey}&fields=files(id,name,thumbnailLink,webContentLink)`
                 );
                 const files = response.data.files;
+                console.log("Files: ", files);
 
                 const formattedPhotos = files.map(file => ({
                     original: file.webContentLink,
@@ -51,22 +40,26 @@ function GalleryComponent() {
     }, []);
 
     return (
-        <div className="mt-[5.1rem] h-screen app">
+        <div className="mt-[5.1rem] min-h-screen G-App">
             <LightGallery
                 onInit={() => console.log('lightGallery has been initialized')}
                 speed={500}
                 plugins={[lgThumbnail, lgZoom, lgAutoplay, lgFullscreen, lgRotate, lgShare]}
             >
                 {photosData.map((image, index) => (
-                    <a href={image.original} key={index}>
-                        <img alt={image.alt} src={image.thumbnail} className='thumblin-img' />
+                    <a href={image.original} key={index} data-src={image.original}>
+                        <img 
+                            alt={image.alt} 
+                            src={image.thumbnail} 
+                            className='thumblin-img' 
+                           
+                        />
                     </a>
                 ))}
             </LightGallery>
         </div>
     );
 }
-
 
 
 
