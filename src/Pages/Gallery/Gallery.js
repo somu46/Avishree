cbc068ad24318bf904128c1cac83153e2a479841
Arkajyoti Kundu <arkajyotikundu415@gmail.com>
2024-11-23 12:18:15 +0530

    "Added axios dependency, updated Gallery component to fetch photos from Google Drive, and modified package.json and package-lock.json files accordingly."

--- src/Pages/Gallery/Gallery.js
+++ src/Pages/Gallery/Gallery.js
@@ -1,4 +1,4 @@
-import PhotosData from '../Photos/PhotoData'; 
+// import PhotosData from '../Photos/PhotoData'; 
 import './GalleryStyles.scss'; 
 
 
@@ -17,90 +17,104 @@ import lgRotate from 'lightgallery/plugins/rotate';
 
 
 
-
-
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function GalleryComponent() {
-    const onInit = () => {
-        console.log('lightGallery has been initialized');
-    };
+    const [photosData, setPhotosData] = useState([]);
+
+    useEffect(() => {
+        // Function to fetch photos from Google Drive
+        const fetchPhotos = async () => {
+            const apiKey = 'YOUR_API_KEY';
+            const folderId = 'YOUR_FOLDER_ID';
+
+            try {
+                const response = await axios.get(
+                    `https://www.googleapis.com/drive/v3/files?q='${folderId}'+in+parents&key=${apiKey}&fields=files(id,name,thumbnailLink,webContentLink)`
+                );
+                const files = response.data.files;
+
+                const formattedPhotos = files.map(file => ({
+                    original: file.webContentLink,
+                    thumbnail: file.thumbnailLink,
+                    alt: file.name
+                }));
+
+                setPhotosData(formattedPhotos);
+            } catch (error) {
+                console.error('Error fetching photos', error);
+            }
+        };
+
+        fetchPhotos();
+    }, []);
+
     return (
-        <div className="mt-[5.1rem] app">
+        <div className="mt-[5.1rem] h-screen app">
             <LightGallery
-                onInit={onInit}
+                onInit={() => console.log('lightGallery has been initialized')}
                 speed={500}
                 plugins={[lgThumbnail, lgZoom, lgAutoplay, lgFullscreen, lgRotate, lgShare]}
             >
-
-                {PhotosData.map((image, index) => {
-                    return (
-                        <a href={image.original} key={index}>
-                            <img alt={image.alt} src={image.thumbnail} className='thumblin-img'/>
-                        </a>
-                    )
-                })}
-
-
+                {photosData.map((image, index) => (
+                    <a href={image.original} key={index}>
+                        <img alt={image.alt} src={image.thumbnail} className='thumblin-img' />
+                    </a>
+                ))}
             </LightGallery>
         </div>
     );
 }
 
-// import React, { useEffect } from 'react';
-// import Masonry from 'masonry-layout';
-// import imagesLoaded from 'imagesloaded';
-// import LightGallery from 'lightgallery/react';
-// import lgZoom from 'lightgallery/plugins/zoom';
-// import lgShare from 'lightgallery/plugins/share';
-// import lgHash from 'lightgallery/plugins/hash';
 
 
 
 
-// const GalleryComponent = () => {
-//   useEffect(() => {
-//     const container = document.querySelector('.masonry-gallery-demo');
-//     if (container) {
-      
-//       const msnry = new Masonry(container, {
-//         itemSelector: '.gallery-item',
-//         columnWidth: '.grid-sizer',
-//         percentPosition: true,
-//       });
 
-      
-//       imagesLoaded(container).on('progress', () => {
-//         msnry.layout();
-//       });
-//     }
-//   }, []);
 
-//   return (
-//     <div className="mt-[5.3rem] border border-red-500 p-3 h-screen">
-//       <LightGallery
-//         elementClassNames="masonry-gallery-demo  "
-//         plugins={[lgZoom, lgShare, lgHash]}
-//         speed={500}
-//       >
-//         <div className=" flex  border border-blue-500 h-auto m-1"></div>
-//         {PhotosData.map((photo) => (
-//           <a
-//             key={photo.id}
-//             href={photo.original}
-//             data-lg-size="1600-1067"
-//             className=" border border-yellow-600"
-//             data-src={photo.original}
-//           >
-//             <img
-//               alt="Gallery poster"
-//               className="img-responsive border border-green-500"
-//               src={photo.thumbnail}
-//             />
-//           </a>
-//         ))}
-//       </LightGallery>
-//     </div>
-//   );
-// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function GalleryComponent() {
+
+
+        
+//     const onInit = () => {
+//         console.log('lightGallery has been initialized');
+//     };
+//     return (
+//         <div className="mt-[5.1rem] app">
+//             <LightGallery
+//                 onInit={onInit}
+//                 speed={500}
+//                 plugins={[lgThumbnail, lgZoom, lgAutoplay, lgFullscreen, lgRotate, lgShare]}
+//             >
+
+//                 {PhotosData.map((image, index) => {
+//                     return (
+//                         <a href={image.original} key={index}>
+//                             <img alt={image.alt} src={image.thumbnail} className='thumblin-img'/>
+//                         </a>
+//                     )
+//                 })}
+
+
+//             </LightGallery>
+//         </div>
+//     );
+// }
+
 
 export default GalleryComponent;
