e8d12c3030eb66e04cc5ce627c9fa7fb7c71ed38
Soumyajit Ray <soumyajitray2002@gmail.com>
2024-11-23 14:28:44 +0530

    "Refactor Expand component: updated imports, modified CSS classes and structure, and added transition effects."

--- src/Components/expand/expand.js
+++ src/Components/expand/expand.js
@@ -1,8 +1,7 @@
-import  { useState } from 'react';
-import { MdOutlineKeyboardDoubleArrowDown ,MdOutlineKeyboardDoubleArrowUp } from "react-icons/md";
+import React, { useState } from 'react';
+import { MdOutlineKeyboardDoubleArrowDown, MdOutlineKeyboardDoubleArrowUp } from "react-icons/md";
 
 const Expand = ({ question, children }) => {
-
   const [isExpanded, setIsExpanded] = useState(false);
 
   const handleToggle = () => {
@@ -10,20 +9,20 @@ const Expand = ({ question, children }) => {
   };
 
   return (
-    <div className="expand-container ">
-      <div className="expand-header" onClick={handleToggle} >
-        <span className='text-red-500 font-mono '>{question}</span>
+    <div className="bg-white p-4 border border-gray-200 rounded-lg shadow-sm mb-4 transition-shadow duration-300 ease-in-out hover:shadow-md cursor-pointer">
+      <div className="flex justify-between items-center" onClick={handleToggle}>
+        <span className="text-red-500 font-mono">{question}</span>
         {isExpanded ? <MdOutlineKeyboardDoubleArrowUp size={25} /> : <MdOutlineKeyboardDoubleArrowDown size={25} />}
       </div>
-
-      {isExpanded && (
-        <div className="expand-content  ">
-          {children}
-        </div>
-      )}
+      <div className={`overflow-hidden transition-max-height duration-1000 ease-in-out ${isExpanded ? 'max-h-screen' : 'max-h-0'}`}>
+        {isExpanded && (
+          <div className="pt-4">
+            {children}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
-
 
 export default Expand;
