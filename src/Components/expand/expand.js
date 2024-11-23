67e322c1f834054d29eb1f29b2a89e0fbbcf976b
Soumyajit Ray <soumyajitray2002@gmail.com>
2024-11-23 22:43:18 +0530

    "Updated CorporateClients.js, IntroSection.js, Services.js, and expand.js with minor changes to text, styling, and functionality."

--- src/Components/expand/expand.js
+++ src/Components/expand/expand.js
@@ -12,8 +12,9 @@ const Expand = ({ question, children }) => {
     <div className="bg-white p-4 border border-gray-200 rounded-lg shadow-sm mb-4 transition-shadow duration-300 ease-in-out hover:shadow-md cursor-pointer">
       <div className="flex justify-between items-center" onClick={handleToggle}>
         <span className="text-red-500 font-mono">{question}</span>
-        {isExpanded ? <MdOutlineKeyboardDoubleArrowUp size={25} /> : <MdOutlineKeyboardDoubleArrowDown size={25} />}
+        {isExpanded ? (<MdOutlineKeyboardDoubleArrowUp size={25} className="font-extrabold" />) : (<MdOutlineKeyboardDoubleArrowDown size={25} className="font-extrabold" />)}
       </div>
+
       <div className={`overflow-hidden transition-max-height duration-1000 ease-in-out ${isExpanded ? 'max-h-screen' : 'max-h-0'}`}>
         {isExpanded && (
           <div className="pt-4">
