58eef0c303b384efb83a625b169d42e053e5dd86
Soumyajit Ray <soumyajitray2002@gmail.com>
2024-11-27 01:07:57 +0530

    "Refactored CorporateClients, Home, and IntroSection components, updated CSS styles, and removed iframe element."

--- src/Components/Home/HomeComponent.js
+++ src/Components/Home/HomeComponent.js
@@ -45,9 +45,6 @@ const HomeCome = () => {
       </div>
       <div>
         <Faq />
-        <div className="m-5 p-5 border border-red-500 flex justify-center">
-          {/* <iframe src="https://drive.google.com/file/d/1EIBTd9bFwB9mx38y5FCwlrxR67HWcH0b/preview" width="640" height="480"></iframe> */}
-        </div>
       </div>
     </div>
   );
