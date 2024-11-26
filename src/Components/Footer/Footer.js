cdaf2d66dab5bd70c83e288ae6a03306c0284b48
Soumyajit Ray <soumyajitray2002@gmail.com>
2024-11-26 12:04:20 +0530

    "Updated noscript tag in index.html, refactored Footer component, and added developer credits in copyright section"

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,4 +1,4 @@
-// import React from "react";
+import React from "react";
 import "./Footer.css";
 import { Link, NavLink } from "react-router-dom";
 import FacebookIcon from "@mui/icons-material/Facebook";
@@ -11,11 +11,9 @@ import ReactTypingEffect from "react-typing-effect";
 
 const Footer = () => {
 
-
-  const handleHistory=()=>{
+  const handleHistory = () => {
     window.scrollTo(0, 0);
   }
-
 
   return (
     <>
@@ -24,17 +22,8 @@ const Footer = () => {
           {/* Company Information */}
           <div className="footer-section company-info">
             <div className="flex justify-center rounded-sm ">
-              {" "}
-              <Link
-              onClick={handleHistory}
-                to='/'
-              >
-              <img
-
-                src={FooterLogo}
-                alt="Company Logo"
-                className="footer-logo rounded-xl"
-              />
+              <Link onClick={handleHistory} to='/'>
+                <img src={FooterLogo} alt="Company Logo" className="footer-logo rounded-xl" />
               </Link>
             </div>
             <p className="text-2xl ">Avishree Hospitality Pvt. Ltd.</p>
@@ -44,7 +33,7 @@ const Footer = () => {
                 eraseSpeed={50}
                 typingDelay={500}
                 eraseDelay={2000}
-                text={["Crafting unfogettable Memories"]}
+                text={["Crafting unforgettable Memories"]}
               />
             </p>
           </div>
@@ -54,95 +43,58 @@ const Footer = () => {
             <h3>Quick Links</h3>
             <ul>
               <li>
-                <NavLink to="/"
-                onClick={handleHistory}
-                >Home</NavLink>
+                <NavLink to="/" onClick={handleHistory}>Home</NavLink>
               </li>
               <li>
-                <NavLink to="/AboutUs"
-                 onClick={handleHistory}
-                 >About Us</NavLink>
+                <NavLink to="/AboutUs" onClick={handleHistory}>About Us</NavLink>
               </li>
               <li>
-                <NavLink to="/services"
-                 onClick={handleHistory}
-                 >Services</NavLink>
+                <NavLink to="/services" onClick={handleHistory}>Services</NavLink>
               </li>
               <li>
-                <NavLink to="/menu"
-                 onClick={handleHistory}
-                 >Menu</NavLink>
+                <NavLink to="/menu" onClick={handleHistory}>Menu</NavLink>
               </li>
               <li>
-                <span 
-                 onClick={handleHistory}
-                 >Gallery</span>
+                <span onClick={handleHistory}>Gallery</span>
               </li>
               <li>
-                <NavLink to="/contact"
-                 onClick={handleHistory}
-                 >Contact Us</NavLink>
+                <NavLink to="/contact" onClick={handleHistory}>Contact Us</NavLink>
               </li>
-              {/* <li>
-                <NavLink to="/blogs">Blogs</NavLink>
-              </li> */}
             </ul>
           </div>
 
           {/* Social Media Links */}
-          <div className="footer-section social  ">
+          <div className="footer-section social">
             <h3 className="text-center">Follow Us</h3>
             <div className="social-icons">
-              <a
-                href="https://www.facebook.com/profile.php?id=61565446652658"
-                target="_blank"
-                rel="noreferrer"
-                className="social-icon"
-              >
+              <a href="https://www.facebook.com/profile.php?id=61565446652658" target="_blank" rel="noreferrer" className="social-icon">
                 <FacebookIcon />
               </a>
-              <a
-                href="https://www.instagram.com/avishreehospitalityofficial/"
-                target="_blank"
-                rel="noreferrer"
-                className="social-icon"
-              >
+              <a href="https://www.instagram.com/avishreehospitalityofficial/" target="_blank" rel="noreferrer" className="social-icon">
                 <InstagramIcon />
               </a>
-              <a
-                href="https://twitter.com"
-                target="_blank"
-                rel="noreferrer"
-                className="social-icon"
-              >
+              <a href="https://twitter.com" target="_blank" rel="noreferrer" className="social-icon">
                 <XIcon />
               </a>
-              <a
-                href="https://www.youtube.com/@avishreehospitality"
-                target="_blank"
-                rel="noreferrer"
-                className="social-icon"
-              >
+              <a href="https://www.youtube.com/@avishreehospitality" target="_blank" rel="noreferrer" className="social-icon">
                 <YouTubeIcon />
               </a>
             </div>
             <div className="h-auto w-auto my-5">
               <h3 className="mb-3 text-center">Scan Me:</h3>
-         
-          <img src={Qrcode} alt="Qr Code" className="h-auto w-32 bg-white  rounded-none" />
-          
+              <img src={Qrcode} alt="Qr Code" className="h-auto w-32 bg-white rounded-none" />
             </div>
           </div>
 
           {/* Newsletter Subscription */}
-          <div className=" flex flex-col justify-start  w-auto">
+          <div className="flex flex-col justify-start w-auto">
             <h3 className="text-yellow-400 text-center m-5">
               Subscribe to Our Newsletter
             </h3>
             <p className="text-gray-300 text-center mb-3">
               Get the latest updates and offers.
             </p>
-            <div className="newsletter-input-container rounded-lg overflow-hidden  w-auto ">
+            <div className="newsletter-input-container rounded-lg overflow-hidden w-auto">
               <input
                 type="email"
                 placeholder="Enter your email"
@@ -153,11 +105,13 @@ const Footer = () => {
           </div>
         </div>
       </footer>
-      <div className="copyRight flex justify-center items-center max-w-[100%] flex-col w-screen  bottom-0 m-0">
+      <div className="copyRight flex justify-center items-center max-w-[100%] flex-col w-screen bottom-0 m-0">
         <div className="border-t-2 border-slate-500 w-full lg:w-[70%]"></div>
         <p className="text-center text-gray-300 font-thin">
-          Copyright 2023 Avishree Hospitality | &copy; 2024-2027 . All rights
-          reserved.{" "}
+          Copyright 2023 Avishree Hospitality | &copy; 2024-2027. All rights reserved. 
+        </p>
+        <p className="text-center text-gray-300 font-thin">
+          Developed by<a href="https://github.com/somu46" className="text-yellow-400"> Soumyajit Ray</a> and <a href="https://github.com/Arkajyoti333" className="text-yellow-400">Arkajyoti Kundu</a>
         </p>
       </div>
     </>
