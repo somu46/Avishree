97543e92c8a10d1e7e579c6c3a3f707d8dee160e
Arkajyoti Kundu <arkajyotikundu415@gmail.com>
2024-11-27 00:32:09 +0530

    "Updated Home component CSS and JS files, modifying hero section height, background image, and layout of various sections."

--- src/Components/Home/HomeComponent.js
+++ src/Components/Home/HomeComponent.js
@@ -1,4 +1,4 @@
-// import React from "react";
+import React from "react";
 import "./Home.css";
 import Feature from "./Feature";
 import Services from "./Services";
@@ -7,66 +7,50 @@ import CorporateClients from "./CorporateClients";
 import StatsSection from "./SpeakingNumbers";
 import IntroSection from "./IntroSection";
 import HowItWorks from "./HowItWork";
-// import Testimonial from "../Testimonial/Testimonial";
 import Faq from "../Faqs/faq";
 import BanquetHalls from "./BanquetHalls";
 import FloatingWhatsAppButton from "../FloatWhatsapp/FloatingWhatsAppButton";
-// import GoogleReviews from '../GoogleReviews/GoogleReviews.js';
-
-
-
-
-
-
 
 
 const HomeCome = () => {
   return (
-    <div className="home-container "
-   
-    >
-      
-        <Hero/>
-        <div  className="bg-image">
+    <div className="home-container ">
+      <Hero />
+      <div className="bg-image mt-[18rem] lg:mt-[9rem] ">
         <div className="max-w-screen bg-transparent opacity-80 backdrop-blur-xl">
-        <IntroSection/>
+          <IntroSection />
         </div>
-        </div>
-<div>
-      <Feature />
-
-<div>
-      <FloatingWhatsAppButton/>
-</div>
-<div>
-      <HowItWorks/>
-</div>
-<div>
-      <Services />
-</div>
-<div>
-      <BanquetHalls/>
-</div>
-<div>
-      <CorporateClients/>
-</div>
-<div>
-      {/* <GoogleReviews/> */}
-</div>
-      {/* <Testimonial/> */}
-      <StatsSection/>
-</div>
- <div>     
-      <Faq/>
-      <div className="m-5 p-5 border border-red-500 flex justify-center">
-      {/* <iframe src="https://drive.google.com/file/d/1EIBTd9bFwB9mx38y5FCwlrxR67HWcH0b/preview" width="640" height="480"></iframe> */}
-
-
-
       </div>
+      <div>
+        <Feature />
 
+        <div>
+          <FloatingWhatsAppButton />
+        </div>
+        <div>
+          <HowItWorks />
+        </div>
+        <div>
+          <Services />
+        </div>
+        <div>
+          <BanquetHalls />
+        </div>
+        <div>
+          <CorporateClients />
+        </div>
+        <div>{/* <GoogleReviews/> */}</div>
+        {/* <Testimonial/> */}
+        <StatsSection />
+      </div>
+      <div>
+        <Faq />
+        <div className="m-5 p-5 border border-red-500 flex justify-center">
+          {/* <iframe src="https://drive.google.com/file/d/1EIBTd9bFwB9mx38y5FCwlrxR67HWcH0b/preview" width="640" height="480"></iframe> */}
+        </div>
+      </div>
     </div>
- </div> );
+  );
 };
 
 export default HomeCome;
